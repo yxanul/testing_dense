350fa1f767791b0f9ac4f41995d99825b2be9d3a
yxanul <davidfranco2300@gmail.com>
2025-09-19 15:44:50 +0300

    modified:   model_te_final_v2.py

--- model_te_final_v2.py
+++ model_te_final_v2.py
@@ -509,9 +509,9 @@ def benchmark_large_scale():
     # Test configurations: (batch_size, seq_len, grad_acc_steps)
     # Focus on larger batches / GA as requested
     test_configs = [
-        (16, 1024, 1),   # 16 BA, 1 GA, 1024 seq
-        (16, 1024, 16),  # 16 BA, 16 GA, 1024 seq (effective BS=256)
-        (24, 1024, 1),   # 24 BA, 1 GA, 1024 seq
+        #(16, 1024, 1),   # 16 BA, 1 GA, 1024 seq
+        #(16, 1024, 16),  # 16 BA, 16 GA, 1024 seq (effective BS=256)
+        #(24, 1024, 1),   # 24 BA, 1 GA, 1024 seq
         (20, 2048, 1),   # 20 BA, 1 GA, 2048 seq
     ]
 
